e1c58f784e94455459ef19f8fc0748cfb9d5f874
nagendrap123 <nagendrap0820@gmail.com>
2024-07-21 19:28:17 +0530

    terraform

--- expense/ec2/variables.tf
+++ expense/ec2/variables.tf
@@ -1,3 +1,4 @@
+#ec2 instance variables
 variable "ami-id" {
     default = "ami-041e2ea9402c46c32"
     description = "RHEL-ami-id"
@@ -16,11 +17,22 @@ variable "instance_names" {
     default = ["db","backend","frontend"]
   
 }
+#security group variables
 variable "ssh_port" {
     default = "22"
   
 }
 variable "ssh_protocol" {
     default = "tcp"
+  
+}
+#route 53 variables
+variable "zone_id" {
+    default = "Z0193540Q6U1VWFRHQ6D"
+  
+}
+
+variable "domain_name" {
+    default = "nagdevops.online"
   
 }
